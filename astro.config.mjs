2333e1ebbc24b8a9dc47cc0cb88a107aec782904
bp-g <108763133+bp-g@users.noreply.github.com>
2023-09-10 13:01:02 -0700

    Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
-  site:'https://benpagegil.com',
+  site: 'https://benpagegil.com',
+  output: "server",
+  adapter: netlify()
 });
