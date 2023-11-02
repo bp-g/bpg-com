7fbfaf69e2f0b22a279230db02d1e639687c8cda
bp-g <108763133+bp-g@users.noreply.github.com>
2023-11-01 17:11:21 -0700

    form

---
+++ .netlify/functions-internal/chunks/index_4db56189.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/index_2f7334bb.mjs');
+
+export { page };
--- .netlify/functions-internal/entry.mjs
+++ .netlify/functions-internal/entry.mjs
@@ -1,6 +1,6 @@
 import * as adapter from '@astrojs/netlify/netlify-functions.js';
 import { renderers } from './renderers.mjs';
-import { manifest } from './manifest_64bd2aec.mjs';
+import { manifest } from './manifest_7af3201d.mjs';
 import 'cookie';
 import 'kleur/colors';
 import 'string-width';
@@ -12,7 +12,7 @@ import 'mime';
 import 'path-to-regexp';
 
 const _page0  = () => import('./chunks/generic_09956bb7.mjs');
-const _page1  = () => import('./chunks/index_1deeab80.mjs');
+const _page1  = () => import('./chunks/index_4db56189.mjs');
 const _page2  = () => import('./chunks/success_25727656.mjs');
 const _page3  = () => import('./chunks/404_5b0f68f9.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/index.astro", _page1],["src/pages/success.astro", _page2],["src/pages/404.astro", _page3]]);
 const _manifest = Object.assign(manifest, {
